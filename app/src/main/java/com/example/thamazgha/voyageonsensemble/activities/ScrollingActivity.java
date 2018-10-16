b5e2fc6131ee1a14edafc98612e29101defbaf72
CharafLachachi <lac.charaf@gmail.com>
2018-10-16 16:54:21 +0200

    Add Register Activity

---
+++ app/src/main/java/com/example/thamazgha/voyageonsensemble/activities/ScrollingActivity.java
@@ -0,0 +1,17 @@
+package com.example.thamazgha.voyageonsensemble.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.thamazgha.voyageonsensemble.R;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+      //  getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
